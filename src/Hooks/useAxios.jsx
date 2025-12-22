651db331e3f701b03851d2daa491bde8ff4908c7
AbdurRob <abdurrobr34l@gmail.com>
2025-12-22 22:00:31 +0600

    Updated

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -5,7 +5,8 @@ import { useNavigate } from "react-router";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://scholar-stream-server-green-nine.vercel.app/",
 });
 
 const useAxios = () => {
@@ -50,51 +51,3 @@ const useAxios = () => {
 };
 
 export default useAxios;
-
-// import axios from "axios";
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router";
-// import useAuth from "./useAuth";
-
-// const axiosSecure = axios.create({
-//   baseURL: "http://localhost:3000",
-// });
-
-// const useAxios = () => {
-//   const { user, logOut } = useAuth();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const reqInterceptor = axiosSecure.interceptors.request.use(
-//       async (config) => {
-//         if (user) {
-//           const token = await user.getIdToken();
-//           config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;
-//       }
-//     );
-
-//     const resInterceptor = axiosSecure.interceptors.response.use(
-//       (res) => res,
-//       (error) => {
-//         const statusCode = error?.response?.status;
-
-//         if (statusCode === 401 || statusCode === 403) {
-//           logOut().then(() => navigate("/login"));
-//         }
-
-//         return Promise.reject(error);
-//       }
-//     );
-
-//     return () => {
-//       axiosSecure.interceptors.request.eject(reqInterceptor);
-//       axiosSecure.interceptors.response.eject(resInterceptor);
-//     };
-//   }, [user, logOut, navigate]);
-
-//   return axiosSecure;
-// };
-
-// export default useAxios;
